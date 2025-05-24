40b7200d4100b6b4e956e02d3567e81bf5800636
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-05-24 09:48:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const path = require('path');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use('/', routes);
 
 app.use((err, req, res, next) => {
